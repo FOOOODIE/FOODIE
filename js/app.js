44199a33f1068473c652f5051d50cd2c6002e6db
Bashar-Owainat <owainatbashar13@gmail.com>
2021-08-24 12:35:41 +0300

    updated js

--- js/app.js
+++ js/app.js
@@ -4,11 +4,12 @@ let item = [];
 let Cart = function(name, price){
   this.name = name;
   this.price = price;
-  // this.quantity = quantity;
   this.tax = 16;
   item.push(this);
   saveToLocalStorage();
 };
+
+
 
 function saveToLocalStorage (){
   let StringItem = JSON.stringify(item);
--- js/cart.js
+++ js/cart.js
@@ -1,72 +1,65 @@
 
-let container =document.getElementById('container');
+// let container =document.getElementById('container');
+
 let billForm = document.getElementById('bill');
-readFromLocalStorage();
-function reander(){
-    let container = document.getElementById('container');
-    let table = document.getElementById('bill')
-    container.appendChild(table);
 
-}
+
+
+// fun to get from local storge
 function readFromLocalStorage() {
-    let stringObj = localStorage.getItem('cart');
-    let normalObj = JSON.parse(stringObj);
-    if (normalObj) {
-      item.push(normalObj);
-    //   console.log(normalObj.name);
-      bill();
-    }
+  let stringObj = localStorage.getItem('cart');
+  let normalObj = JSON.parse(stringObj);
+  if (normalObj) {
+    item = normalObj;
+    renderCartList();
   }
+}
 
-function bill(){
+function renderCartList() {
   createTableHeader();
-  container.appendChild(billForm); 
-  
+  // container.appendChild(billForm); 
+  let total = 0
+
+  for (let i = 0; i < item.length; i++) {
+    total+=item[i].price
+
+    let trEl = document.createElement('tr');
+    billForm.appendChild(trEl);
+
+    let tdEl1 = document.createElement('td');
+    trEl.appendChild(tdEl1);
+    tdEl1.textContent = item[i].name;
+
+    let tdEl11 = document.createElement('td');
+    trEl.appendChild(tdEl11);
+    tdEl11.textContent = item[i].price;
+  }
   let trEl = document.createElement('tr');
   billForm.appendChild(trEl);
 
   let tdEl1 = document.createElement('td');
   trEl.appendChild(tdEl1);
-  tdEl1.textContent = item[0][0].name;
+  tdEl1.textContent = 'Total';
 
   let tdEl11 = document.createElement('td');
   trEl.appendChild(tdEl11);
-  tdEl11.textContent = item[0][0].price;
+  tdEl11.textContent = total;
 
-  let tdEl2 = document.createElement('td');
-  trEl.appendChild(tdEl2);
-  tdEl2.textContent = item[0][1].name;
+}
 
-  let tdEl22 = document.createElement('td');
-  trEl.appendChild(tdEl22);
-  tdEl22.textContent = item[0][1].price;
+function createTableHeader() {
+  let trEl = document.createElement('tr');
+  billForm.appendChild(trEl);
 
-  let tdEl3 = document.createElement('td');
-  trEl.appendChild(tdEl3);
-  tdEl3.textContent = item[0][2].name;
-  let tdEl33 = document.createElement('td');
-  tdEl33.textContent = item[0][2].price;
-  trEl.appendChild(tdEl33);
-  // for(let i =0; i< item.length; i++){
-  //   // thEl.textContent = '';
-  //     thEl.textContent = `name: ${item[i][0].name}`;
-  //     thEl2.textContent = `name: ${item[i][1].name}`;
-  //     thEl3.textContent = `name: ${item[i][2].name}`;
-  //     // thEl.textContent = `price: ${item[i][2].price}`;
-  // }
-  }
+  let thEl1 = document.createElement('th');
+  trEl.appendChild(thEl1);
+  thEl1.textContent = 'Meals Name';
 
-  function createTableHeader() {
-    let trEl = document.createElement('tr');
-    billForm.appendChild(trEl);
-
-    let thEl1 = document.createElement('th');
-    trEl.appendChild(thEl1);
-    thEl1.textContent = 'Meals Name';
-
-    let thEl2 = document.createElement('th');
-    trEl.appendChild(thEl2);
-    thEl2.textContent = 'price';
-  }
+  let thEl2 = document.createElement('th');
+  trEl.appendChild(thEl2);
+  thEl2.textContent = 'price';
+}
 
 // console.log(item);
+readFromLocalStorage()
+console.log(item);
