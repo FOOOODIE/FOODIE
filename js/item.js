5614f4c2cc4a4ab497b89b883490d9974942e372
Hesham-Hassan9 <19012748@student.ltuc.com>
2021-08-25 14:43:50 +0300

    edite js

--- js/item.js
+++ js/item.js
@@ -1,47 +1,34 @@
 'use strict';
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', ready);
-} else {
-  ready();
-}
+// if (document.readyState === 'loading') {
+//   document.addEventListener('DOMContentLoaded', ready);
+// } else {
+//   ready();
+// }
 
 function ready() {
-  let addToCartButtons = document.getElementsByClassName('add-to-cart');
+  let addToCartButtons = document.querySelectorAll('.add-to-cart');
   for (let i = 0; i < addToCartButtons.length; i++) {
     let button = addToCartButtons[i];
     button.addEventListener('click', addToCartClicked);
     
   }
-
 }
+ready()
 
-function quantityChanged() {
-  let quantity =+1 ;
-  return quantity;
- 
-}
+
 
 function addToCartClicked(event) {
-  let button = event.target;
-  let shopItem = button.parentElement;
-  let title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
-  let price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
-  addItemToCart(title, price.split(' ')[0]);
+  console.log(event);
+  // let button = event.target;
+  // let shopItem = button.parentElement;
+  // let title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
+  // let price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
+  // addItemToCart(title, price.split(' ')[0]);
+  // console.log(`name : ${title}`, ` : ${price}`);
+  // new Cart(title, price.split(' ')[0])
+  // console.log(`all items :`,item);
 }
-let quantity
-function addItemToCart(title, price) {
-  // let quantity = quantityChanged();
-  new Cart(title, price);
+// function addItemToCart(title, price) {
+//   new Cart(title, price);
   
-}
-// function readFromLocalStorage() {
-//   let stringObj = localStorage.getItem('cart');
-//   let normalObj = JSON.parse(stringObj);
-
-//   if (normalObj) {
-//     item.push(normalObj);
-
-//     // showResults();
-//   }
 // }
-// readFromLocalStorage();
